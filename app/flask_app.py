cd72987562fed45077d12da4c46838ebf111887b
stormcorgi <pirete@black-pearl>
2022-02-26 16:15:22 +0000

    pdf viewer and fullscreen, port change for pict-mode

--- app/flask_app.py
+++ app/flask_app.py
@@ -48,4 +48,4 @@ def rend_player():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000, threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=9000, threaded=True)
