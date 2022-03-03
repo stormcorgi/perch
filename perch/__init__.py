6c36a6a21cdcb581ee72160df5d7d875eb8c3776
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2022-03-03 12:34:40 +0900

    use "flask run", add app fixture

---
+++ perch/__init__.py
@@ -0,0 +1,62 @@
+""" use flask and perchdb.py """
+import os
+from flask import render_template, Flask, request
+from perch import perchdb as db
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of flask app"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, "/database/perch.db")
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.update(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def rend_main():
+        """render main page"""
+        return render_template(
+            "main.html",
+            actresses=db.Actress.all(),
+            tags=db.Tag.all()
+        )
+
+    @app.route("/actress/<name>")
+    def rend_aectress(name):
+        """render actress page"""
+        actress = db.Actress.get_by_name(name)
+        movies = db.Movie.get_by_actress(actress.actressid)
+        return render_template("actress.html", actress=actress, movies=movies)
+
+    @app.route("/tag/<tag>")
+    def rend_tag(tag):
+        """render tag page"""
+        movies = db.Movie.get_by_tag(tag)
+        return render_template("tags.html", tag=tag, movies=movies)
+
+    @app.route("/player")
+    def rend_player():
+        """render player page"""
+        fileid = request.args.get('id', default=None, type=str)
+        filepath = fileid + ".info"
+        filename = request.args.get('name', default=None, type=str)
+        tags = db.Tag.get_by_movie(fileid)
+        actresses = db.Actress.get_by_movie(fileid)
+        return render_template(
+            "player.html",
+            filepath=filepath,
+            filename=filename,
+            actresses=actresses,
+            tags=tags
+        )
+
+    return app
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,37 @@
 
 import sys
 import os
+import pytest
+import tempfile
+from perch import create_app
 
 sys.path.append(os.path.abspath(os.path.dirname(
     os.path.abspath(__file__)) + "/../app/"))
 
 SAMPLE_LIB = "./tests/sample.library"
 SAMPLE_DB_PATH = 'sqlite:///tests/test.db'
+
+
+@pytest.fixture(name="app")
+def fixture_app():
+    """Create and configure a new app instance for each test."""
+    # create a temporary file to isolate the database for each test
+    db_fd, db_path = tempfile.mkstemp()
+    # create the app with common test config
+    app = create_app({"TESTING": True, "DATABASE": db_path})
+
+    # create the database and load test data
+    # with app.app_context():
+    #     init_db()
+    #     get_db().executescript(_data_sql)
+
+    yield app
+
+    # close and remove the temporary database
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
