6deee18d9713608b513454140ff3707868a074bb
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2023-09-10 18:44:58 +0900

    add sample data on top / use those data to develop

--- application/list/folder.py
+++ application/list/folder.py
@@ -1,43 +1,20 @@
 from flask import Blueprint, redirect, url_for, render_template
-from perch.repository.MemRepo import MemActressRepo, MemMovieRepo
+from perch.repository.MemRepo import MemFolderRepo, MemItemRepo
+from application.list.top import data_folder, data_item
+
 
 blueprint = Blueprint("folder", __name__)
-
-
-def actress_dicts():
-    return [
-        {
-            "id": "KYBJABI93CWTJ",
-            "name": "新垣結衣",
-            "description": "",
-            "children": [],
-            "modificationTime": 1641991403459,
-            "tags": [],
-            "password": "",
-            "passwordTips": "",
-        },
-        {
-            "id": "KYBK5NZTEWDMP",
-            "name": "伊織もえ",
-            "description": "",
-            "children": [],
-            "modificationTime": 1641992866012,
-            "tags": [],
-            "password": "",
-            "passwordTips": "",
-        },
-    ]
 
 
 @blueprint.route("/actress/<name>")
 def rend_actress(name):
     """render actress page"""
-    repoActress = MemActressRepo(actress_dicts())
-    repoMovie = MemMovieRepo({})
+    repoActress = MemFolderRepo(data_folder)
+    repoMovie = MemItemRepo(data_item)
     actress = repoActress.search_by_name(name)
     if not actress:
         return redirect(url_for("rend_main"))
-    movies = repoMovie.search_by_actress(actress)
+    movies = repoMovie.search_by_folder(actress)
     return render_template(
         "actress.html",
         actress=actress,
--- application/list/top.py
+++ application/list/top.py
@@ -1,41 +1,50 @@
-from flask import Blueprint, redirect, url_for, render_template
-from perch.repository.MemRepo import MemActressRepo, MemMovieRepo
+from flask import Blueprint, render_template
+from perch.use_cases.ActressList import actress_list_use_case
+from perch.repository.MemRepo import MemFolderRepo
+
 
 blueprint = Blueprint("top", __name__)
 
 
-def actress_dicts():
-    return [
-        {
-            "id": "KYBJABI93CWTJ",
-            "name": "新垣結衣",
-            "description": "",
-            "children": [],
-            "modificationTime": 1641991403459,
-            "tags": [],
-            "password": "",
-            "passwordTips": "",
-        },
-        {
-            "id": "KYBK5NZTEWDMP",
-            "name": "伊織もえ",
-            "description": "",
-            "children": [],
-            "modificationTime": 1641992866012,
-            "tags": [],
-            "password": "",
-            "passwordTips": "",
-        },
-    ]
+data_folder = [
+    {
+        "id": "LHL9WA8UH3NSJ",
+        "name": "Norah Jones",
+        "description": "Academy Award-winning actress known for her versatile performances.",
+        "children": [],
+        "modificationTime": 16567890,
+        "tags": ["singer", "songwriter", "pianist"],
+        "password": "S3cr3tP@ssw0rd",
+        "passwordTips": "Remember to include special characters and numbers in your password.",
+    },
+]
+
+data_item = [
+    {
+        "id": "LHLC3F8KFALUE",
+        "name": "test",
+        "size": 2878774335,
+        "btime": 1683942778000,
+        "mtime": 1683942814000,
+        "ext": "mp4",
+        "tags": [],
+        "folders": ["LHL9WA8UH3NSJ"],
+        "isDeleted": False,
+        "url": "",
+        "annotation": "",
+        "modificationTime": 1683942880687,
+        "duration": 7432.512,
+        "height": 1080,
+        "width": 1920,
+        "lastModified": 1683942945589,
+        "palettes": [{"color": [4, 4, 4], "ratio": 100}],
+    }
+]
 
 
 @blueprint.route("/")
 def render_main():
     """render main page"""
-    repoActress = MemActressRepo(actress_dicts())
-    return render_template(
-        "main.html",
-        actresses=repoActress.list(),
-        tags={}
-        # tags=dbcon.Tag.all(current_session),
-    )
+
+    actress_list = actress_list_use_case(MemFolderRepo(data_folder))
+    return render_template("main.html", actresses=actress_list, tags={})
